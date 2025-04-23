08a6a1722ce615c849aeab2669449481b3bfb05a
Vishvadeep Singh Chauhan <vishvadeepsinghchauhan@gmail.com>
2025-04-23 18:44:43 +0530

    react

---
+++ 04-custom-React/costumReact.js
@@ -0,0 +1,14 @@
+function customRender() {}
+
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  children: "click me to visit google",
+};
+
+const mainContainer = document.querySelector("#root");
+
+customRender(reactElement, mainContainer);
